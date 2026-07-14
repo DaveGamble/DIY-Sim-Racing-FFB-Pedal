932b072e16fbdbc8ca2c79f9727a81830bb7d011
Dave Gamble <davegamble@gmail.com>
2026-07-14 17:44:31 +0100

    icos to inline

--- ESP32/include/FastTrig.h
+++ ESP32/include/FastTrig.h
@@ -7,51 +7,6 @@
 //    DATE: 2011-08-18
 //     URL: https://github.com/RobTillaart/FastTrig
 //          https://forum.arduino.cc/index.php?topic=69723.0
+// HEAVILY MODIFIED
 
-
-#ifdef ESP_PLATFORM
-#include <math.h>
-#include <stdint.h>
-#include <stdbool.h>
-#else
-#include "Arduino.h"
-#endif
-
-
-#define FAST_TRIG_LIB_VERSION             (F("0.3.6"))
-
-
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-
-///////////////////////////////////////////////////////
-//
-//  GONIO LOOKUP
-//
-float isin(float f);
-float icos(float x);
-
-///////////////////////////////////////////////////////
-//
-//  INVERSE GONIO LOOKUP
-//
-float iacos(float f);
-
-//  PLACEHOLDER (might be obsolete due to atanFast() formula.
-//float iatan(float f);
-//  fast atan() formula, in fact a modified Taylor expansion
-//  input = -1 .. 1
-//float atanFast(float f);
-//  atan2Fast() folds and mirrors => calls atanFast() + offset.
-float atan2Fast(float y, float x);
-
-
-#ifdef __cplusplus
-}
-#endif
-
-
-//  -- END OF FILE --
+float isin(float f); // sin(x), parameters are in degrees
